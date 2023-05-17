ce6755c922efd1bdb639b30beacd365e1a8d9dd2
CarlCarnmo <carl.carnmo@gmail.com>
2023-05-17 14:17:44 +0200

    Update config.py
    Added some configs for gpio pins and psql connection. should work.. probably.

---
+++ config.py
@@ -0,0 +1,12 @@
+# GPIO Pins
+RFID_IO = 19
+ESD_IO = 26
+RED_LED = 16
+YELLOW_LED = 20
+GREEN_LED = 21
+
+# PSQL-Database
+psql_host = "localhost"
+psql_database = "pioneer"
+psql_user = "pioneer"
+psql_password = "password"
