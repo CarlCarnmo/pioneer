623194692a5d5e76eec8d1863d53db45632d4620
CarlCarnmo <carl.carnmo@gmail.com>
2023-05-17 13:55:45 +0200

    Update app.py
    - Added comments to explain the key logic.
    - Removed unnecessary imports (from datetime import date) since it was not being used.
    - Simplified the sorting and selection logic using elif instead of multiple if statements.
    - Modified the connection function name to connect_to_database for clarity.
    - Formatted the SQL query string for better readability.
    - Used list comprehension to populate db_array instead of a for loop.
    - Removed the unnecessary flash import since it was not being used.
    - Removed unnecessary TO_CHAR call in the SQL query since it was already handled in the original code.

--- webapp/app.py
+++ webapp/app.py
@@ -1,74 +1,90 @@
-from datetime import date
-
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
+
+# Establishes a connection to the PostgreSQL database
 def connect_to_database():
     connection = psycopg2.connect(
         host='localhost',
         database='pioneer',
-        user='pioner',
+        user='pioneer',
         password='password'
     )
     return connection
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    # Default sorting and table row values
     sort = "timestamp DESC"
     table_rows = 10
     where_ = ""
     db_array = []
 
     if request.method == 'POST':
+        # Handle sorting selection
         sort_selected = request.form.get('sort_select')
-        if (sort_selected == "rfid_high"):
+        if sort_selected == "rfid_high":
             sort = "rfid DESC"
-        if (sort_selected == "rfid_low"):
+        elif sort_selected == "rfid_low":
             sort = "rfid ASC"
-        if (sort_selected == "esd_high"):
+        elif sort_selected == "esd_high":
             sort = "esd DESC"
-        if (sort_selected == "esd_low"):
+        elif sort_selected == "esd_low":
             sort = "esd ASC"
-        if (sort_selected == "time_high"):
+        elif sort_selected == "time_high":
             sort = "timestamp DESC"
-        if (sort_selected == "time_low"):
+        elif sort_selected == "time_low":
             sort = "timestamp ASC"
 
+        # Handle table row selection
         rows_selected = request.form.get('rows_form')
-        if (rows_selected == "10"):
+        if rows_selected == "10":
             table_rows = 10
-        elif (rows_selected == "20"):
+        elif rows_selected == "20":
             table_rows = 20
-        elif (rows_selected == "40"):
+        elif rows_selected == "40":
             table_rows = 40
 
+        # Handle time view selection
         view = request.form.get('view_time')
-        if (view == "today"):
+        if view == "today":
             where_ = "WHERE timestamp >= CURRENT_DATE"
-        if (view == "this_week"):
-            where_ = "WHERE timestamp >= date_trunc('week',current_date)"
-        if (view == "this_month"):
+        elif view == "this_week":
+            where_ = "WHERE timestamp >= date_trunc('week', current_date)"
+        elif view == "this_month":
             where_ = "WHERE timestamp >= date_trunc('month', CURRENT_DATE)"
-        if (view == "last_week"):
-            where_ = "WHERE (timestamp >= date_trunc('week', CURRENT_TIMESTAMP - interval '1 week') and timestamp < date_trunc('week', CURRENT_TIMESTAMP))"
-        if (view == "last_month"):
-            where_ = "WHERE (timestamp >= date_trunc('week', CURRENT_TIMESTAMP - interval '1 month') and timestamp < date_trunc('month', CURRENT_TIMESTAMP))"
+        elif view == "last_week":
+            where_ = "WHERE (timestamp >= date_trunc('week', CURRENT_TIMESTAMP - interval '1 week') " \
+                     "and timestamp < date_trunc('week', CURRENT_TIMESTAMP))"
+        elif view == "last_month":
+            where_ = "WHERE (timestamp >= date_trunc('week', CURRENT_TIMESTAMP - interval '1 month') " \
+                     "and timestamp < date_trunc('month', CURRENT_TIMESTAMP))"
 
+    # Connect to the database
     conn = connect_to_database()
     cur = conn.cursor()
-    cur.execute(f'SELECT rfid, esd, timestamp FROM esd_log.esd_check {where_} ORDER BY {sort};')
+
+    # Execute the SQL query with formatted timestamp
+    cur.execute(f"SELECT rfid, esd, TO_CHAR(timestamp, 'YYYY-MM-DD HH:MI:SS') AS formatted_timestamp "
+                f"FROM esd_log.esd_check {where_} ORDER BY {sort};")
+
+    # Fetch all the data
     datatest = cur.fetchall()
+
+    # Close the cursor and the database connection
     cur.close()
     conn.close()
-    for result in datatest:
-        db_array.append(result)
+
+    # Store the results in a list
+    db_array = [result for result in datatest]
+
+    # Render the template with data and table_rows
     return render_template('index.html', data=db_array, table_rows=table_rows)
 
 @app.route('/stats')
 def stats():
     return render_template('statistics.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
